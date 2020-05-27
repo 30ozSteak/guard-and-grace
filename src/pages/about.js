7856659cf2a5c34fe9de06fbecaf2d01b1c869ea
Nick Dambrosio <Nick.DAmbrosio@regiscompany.com>
2020-05-27 16:25:28 -0600

    add pages, routes

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>Nick Dambrosio</h1>
+      <h1>This is About Me</h1>
+      <p>Paragraph</p>
+    </div>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/projects/teller.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+const Teller = () => (
+  <Layout>
+    <div>
+      <h1>Teller</h1>
+      <h1>Pretty Cool</h1>
+      <p>Paragraph</p>
+    </div>
+  </Layout>
+)
+
+export default Teller
