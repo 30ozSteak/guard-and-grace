65ff292f3acddb0722433bc065bd91e99abe40f8
Nick Dambrosio <Nick.DAmbrosio@regiscompany.com>
2020-05-27 22:59:07 -0600

    implement query routing for archive of blogs

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Archive from "./archive"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -28,6 +29,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
+        <Archive />
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
